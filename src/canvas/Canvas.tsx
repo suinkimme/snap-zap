feaad242c523c8f7a2f300b14589d3a897c58eb6
suinkim <contact@suinkim.me>
2024-09-12 00:47:57 +0900

    feature: 이미지 붙여 넣기 기능

--- src/canvas/Canvas.tsx
+++ src/canvas/Canvas.tsx
@@ -3,19 +3,53 @@ import { useEffect, useRef } from "react";
 export default function Canvas(
   props: React.CanvasHTMLAttributes<HTMLCanvasElement>
 ) {
-  const ref = useRef<HTMLCanvasElement>(null);
+  const ref = useRef<HTMLCanvasElement | null>(null);
 
-  const draw = (context: CanvasRenderingContext2D) => {
-    context.fillStyle = "red";
-    context.fillRect(10, 10, 100, 100);
+  // 이미지를 붙여넣기 위한 함수
+  const pasteImageFromClipboard = (event: ClipboardEvent) => {
+    const items = event.clipboardData?.items;
+
+    if (!items) return;
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].type.indexOf("image") !== -1) {
+        const blob = items[i].getAsFile();
+        const img = new Image();
+
+        img.onload = () => {
+          const canvas = ref.current;
+          const context = canvas?.getContext("2d");
+          if (canvas && context) {
+            const { width: cwidth, height: cheight } = canvas;
+            const centerX = cwidth / 2;
+            const centerY = cheight / 2;
+            const startX = centerX - img.width / 2;
+            const startY = centerY - img.height / 2;
+
+            context.clearRect(0, 0, cwidth, cheight);
+            context.drawImage(img, startX, startY, img.width, img.height);
+          }
+        };
+
+        if (blob) {
+          img.src = URL.createObjectURL(blob);
+        }
+
+        break;
+      }
+    }
   };
 
   useEffect(() => {
-    const canvas = ref.current;
-    const context = canvas?.getContext("2d");
-    if (context) {
-      draw(context);
-    }
+    const handlePaste = (event: ClipboardEvent) => {
+      pasteImageFromClipboard(event);
+    };
+
+    window.addEventListener("paste", handlePaste);
+
+    return () => {
+      window.removeEventListener("paste", handlePaste);
+    };
   }, []);
 
   return <canvas ref={ref} {...props} />;
