0012c5373d03792fe606836052fa2ad52873831a
suinkim <contact@suinkim.me>
2024-09-15 17:50:20 +0900

    feat: prettier 적용 및 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -25,4 +25,8 @@ export default tseslint.config(
       ],
     },
   },
-)
+  {
+    files: ['**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
+    extends: ['plugin:storybook/recommended'],
+  },
+);
